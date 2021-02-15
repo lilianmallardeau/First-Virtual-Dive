e1bd0966cf55324454c24bdf3fb18322554c781a
Vivien <gagliano.vivien@gmail.com>
2021-02-15 17:39:00 +0100

    Basic player controller with move function

---
+++ Assets/Scripts/Mayo/PlayerControl.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TreeEditor;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class PlayerControl : MonoBehaviour
+{
+    [SerializeField] private XRNode _xrNode = XRNode.RightHand;
+    [SerializeField] private GameObject bodyToMove;
+    public float mvtSpeed = 10f;
+
+    private List<InputDevice> devices = new List<InputDevice>();
+    private InputDevice device;
+
+    private void GetDevices()
+    {
+        InputDevices.GetDevicesAtXRNode(_xrNode, devices);
+        device = devices.FirstOrDefault();
+        Debug.LogError($"Controlled device is {device}");
+    }
+    
+    void Start()
+    {
+        GetDevices();
+    }
+
+    void Update()
+    {
+        if (!device.isValid)
+        {
+            GetDevices();
+        }
+        
+        if (device.TryGetFeatureValue(CommonUsages.triggerButton, out var triggerButtonAction) && triggerButtonAction)
+        {
+            Move();
+        }
+    }
+
+    private void Move()
+    {
+        bodyToMove.transform.Translate(transform.forward * (mvtSpeed * Time.deltaTime));
+    }
+}
