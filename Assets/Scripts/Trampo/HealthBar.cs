9de27152f87a79c987d0691fc6109f3af6ebe0a8
Erwann Debacker <erwann.debacker@gmail.com>
2021-02-23 21:32:01 +0100

    Trampo
    Ajout de la scène UI

---
+++ Assets/Scripts/Trampo/HealthBar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] Image bar;
+    [SerializeField] Text value;
+    public float healthValue = 100f;
+    public Gradient gradient;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) healthValue -= 10f;
+        HealthChange(healthValue);   
+    }
+
+    void HealthChange(float healthValue)
+    {
+        float amount = healthValue / 100f;
+        value.text = healthValue.ToString() + "%";
+        bar.fillAmount = amount;
+        bar.color = gradient.Evaluate(amount);
+    }
+}
