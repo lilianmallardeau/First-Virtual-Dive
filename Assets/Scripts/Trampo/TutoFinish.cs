4be28710b018fbdcba5363d52fdefb27c335d95a
Erwann Debacker <erwann.debacker@gmail.com>
2021-03-05 10:00:34 +0100

    voix début scène principale

---
+++ Assets/Scripts/Trampo/TutoFinish.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutoFinish : MonoBehaviour
+{
+
+    public static bool tutoFinish = true;
+    [SerializeField] private AudioSource melo;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (tutoFinish)
+        {
+            melo.Play();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
