00f78d2c87ebeb4654cff0e8c99928408179e4e8
Erwann Debacker <erwann.debacker@gmail.com>
2021-03-21 18:34:45 +0100

    Oye Oye voilà

---
+++ Assets/Scripts/Trampo/CanvaMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvaMenu : MonoBehaviour
+{
+
+    [SerializeField] private Camera player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(transform.position + player.transform.rotation * Vector3.forward, player.transform.rotation * Vector3.up);
+    }
+}
